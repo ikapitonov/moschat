8952238a18a26182c9ba0f942b97cd2b83032f2f
g3Spad0 <ilyakapitonovspad20@gmail.com>
2020-04-06 03:12:08 +0300

    restart

--- src/main/java/ru/chat/Application.java
+++ src/main/java/ru/chat/Application.java
@@ -3,11 +3,14 @@ package ru.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         System.setProperty("user.timezone", "Europe/Moscow");
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
 
         SpringApplication.run(Application.class, args);
     }
