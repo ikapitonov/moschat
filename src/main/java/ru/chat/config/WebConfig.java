5f5bf687493d3584d9fac7364a24e68685763846
g3Spad0 <ilyakapitonovspad20@gmail.com>
2020-04-14 21:47:44 +0300

    p

--- src/main/java/ru/chat/config/WebConfig.java
+++ src/main/java/ru/chat/config/WebConfig.java
@@ -1,16 +1,16 @@
-//package ru.chat.config;
+package ru.chat.config;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig extends WebMvcConfigurerAdapter {
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("*");
-//    }
-//;}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+}
